9631b976d853ac9f182139d34d8cc6d76ba43d33
bogdan.lucaci <bogdan.lucaci@smart2payts.onmicrosoft.com>
2024-11-29 12:30:10 +0200

    fixed loader fade in/out animations

--- public/js/script.js
+++ public/js/script.js
@@ -2,15 +2,13 @@ const loader = {
     show: () => {
         const _loader = document.querySelector('.loader_mask')
         _loader.classList.remove('fadeOut')
-        _loader.style.display = 'flex'
+        _loader.classList.add('fadeIn')
+        // _loader.style.display = 'flex'
     },
     hide: () => {
         const _loader = document.querySelector('.loader_mask')
+        _loader.classList.remove('fadeIn')
         _loader.classList.add('fadeOut')
         //_loader.style.display = 'none'
     }
 }
-
-const onDocumentReady = (() => {
-    loader.hide()
-})()
